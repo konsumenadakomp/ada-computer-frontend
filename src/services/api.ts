7132288f42459d10cab4e5120c68666182fbce4d
konsumenadakomp <konsumenadakomp@gmail.com>
2025-05-19 09:54:49 +0700

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,5 @@
-const API_URL = 'http://localhost:5001/api';
+   const API_URL = `${import.meta.env.VITE_API_URL}/api` || 'http://localhost:5001/api';
+   console.log('API URL:', API_URL);
 
 export interface ServiceData {
   serviceNumber: string;
